067b4a00fd3458c7175a440a195656a14bdaac94
“Alexandre <dev.alexandre.lopes@gmail.com>
2021-01-19 16:47:57 -0400

    Atualizando o README

--- src/js/exe_02.js
+++ src/js/exe_02.js
@@ -1,10 +1,10 @@
 /* Exercício 01:
-Digite dois números, realize a operação de adição e apresente o resultado com a função alert
+Digite dois números, realize a operação de adição e apresente o resultado
 */
 
-let number1 = prompt('Digite o primeiro número:');
-let number2 = prompt('Digite o segundo número:');
-number1 = parseFloat(number1);
-number2 = parseFloat(number2);
+let number1 = 5
+let number2 = 6
 const resultado =  (number1 + number2);
-alert(`O resultado da soma é: ${resultado}`);
+console.log(resultado);
+
+//Modificado o código
